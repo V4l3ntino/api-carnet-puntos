df6e1671baf664c938a1d6d36e1cd433ed74719f
V4l3ntino <valentinohontar15@gmail.com>
2025-02-17 17:35:58 +0100

    limpio lineas de código sin usar

--- src/incidencia/incidencia.resolver.ts
+++ src/incidencia/incidencia.resolver.ts
@@ -1,8 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { IncidenciaService } from './incidencia.service';
 import { Incidencia } from './entities/incidencia.entity';
-import { CreateIncidenciaDto } from './dto/create-incidencia.dto';
-import { newMessage } from 'functions/functions';
 
 @Resolver()
 export class IncidenciaResolver {
